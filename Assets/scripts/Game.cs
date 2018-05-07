e9b0e1f6877ce68ae5203ed3f62ee772d800db41
Josue Villasante <javillasantet@gmail.com>
2018-05-07 02:07:02 -0500

    added activdades del 11 al 20. firebase conection.

--- Assets/scripts/Game.cs
+++ Assets/scripts/Game.cs
@@ -3,13 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
 
 public class Game : MonoBehaviour {
 
-    public Button leftButton;
-    public Button middleButton;
-    public Button rightButton;
+    public enum Mode { DIEZ, VIENTE }
+
+    public Image leftImage;
+    public Image middleImage;
+    public Image rightImage;
     public Text text;
     public System.Random rnd;
     public AudioSource audioUno;
@@ -25,10 +30,18 @@ public class Game : MonoBehaviour {
     public AudioSource audioCorrecto;
     public AudioSource audioFallo;
 
+    public Mode modo;
+
     private Answer[] availableAnswers;
-    private Button[] buttons;
+    private Image[] images;
     private int correctPosition;
     private AudioSource source;
+    private Boolean isAudioPlaying = false;
+
+    private float time;
+    private string timerString;
+
+    private DatabaseReference currentGameReference;
 
     //trying
     void Awake()
@@ -40,28 +53,55 @@ public class Game : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        rnd = new System.Random();
-        buttons = new Button[3] { leftButton, middleButton, rightButton };
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://youcanadd-ffbe4.firebaseio.com");
 
-        availableAnswers = new Answer[10] {
-            new Answer("unapelotita", "1"),
-            new Answer("dospatitos", "2"),
-            new Answer("trespanditas", "3"),
-            new Answer("cuatromariposas", "4"),
-            new Answer("cincoestrellitas", "5"),
-            new Answer("seisperritos", "6"),
-            new Answer("sietebuhos", "7"),
-            new Answer("ochoabejitas", "8"),
-            new Answer("nueveoreos", "9"),
-            new Answer("diezcandy", "10")};
+        rnd = new System.Random();
+        this.images = new Image[3] { leftImage, middleImage, rightImage };
+
+        if (this.modo == Mode.DIEZ)
+        {
+            availableAnswers = new Answer[10] {
+                new Answer("unapelotita", "1",  audioUno),
+                new Answer("dospatitos", "2", audioDos),
+                new Answer("trespanditas", "3", audioTres),
+                new Answer("cuatromariposas", "4", audioCuatro),
+                new Answer("cincoestrellitas", "5", audioCinco),
+                new Answer("seisperritos", "6", audioSeis),
+                new Answer("sietebuhos", "7", audioSiete),
+                new Answer("ochoabejitas", "8", audioOcho),
+                new Answer("nueveoreos", "9", audioNueve),
+                new Answer("diezcandy", "10", audioDiez)
+            };
+        }
+        else if (this.modo == Mode.VIENTE)
+        {
+            availableAnswers = new Answer[10] {
+                new Answer("onceositos", "11",  audioUno),
+                new Answer("docepeces", "12", audioDos),
+                new Answer("trecesapitos", "13", audioTres),
+                new Answer("catorcecerditos", "14", audioCuatro),
+                new Answer("quincecarritos", "15", audioCinco),
+                new Answer("dieciseisnaranjas", "16", audioSeis),
+                new Answer("diecisietetortugas", "17", audioSiete),
+                new Answer("dieciochofresas", "18", audioOcho),
+                new Answer("diecinuevecupcake", "19", audioNueve),
+                new Answer("veintecorazones", "20", audioDiez)
+            };
+        }
+        
 
         this.Next();
+        time = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        time += Time.deltaTime;
+        int minutes = (int)(time / 60);
+        int seconds = (int)(time % 60);
+        int miliseconds = (int)(time * 1000) % 1000;
+        timerString = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, miliseconds);
     }
 
     public void Next()
@@ -69,10 +109,16 @@ public class Game : MonoBehaviour {
         List<Answer> answers = getRandomUnreapeatedAnswers();
         for (int i = 0; i < 3; i++)
         {
-            buttons[i].image.sprite = Resources.Load<Sprite>(answers[i].image);
+            images[i].sprite = Resources.Load<Sprite>(answers[i].image);
         }
         correctPosition = rnd.Next(3);
         text.text = answers[correctPosition].name;
+        answers[correctPosition].audio.Play();
+        time = 0;
+
+        currentGameReference = FirebaseDatabase.DefaultInstance.RootReference.Child("games").Push();
+        currentGameReference.Child("fecha").SetValueAsync(DateTime.Now.ToString());
+        currentGameReference.Child("numero").SetValueAsync(answers[correctPosition].name);
     }
 
     private List<Answer> getRandomUnreapeatedAnswers()
@@ -90,46 +136,74 @@ public class Game : MonoBehaviour {
 
     public void OnLeftClick()
     {
-        if (correctPosition == 0)
+        if (!this.isAudioPlaying)
         {
-            audioCorrecto.Play();
-            StartCoroutine(DoNextAfterDelay(1.488f));
-        }
-    }
-
-    IEnumerator DoNextAfterDelay(float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        Next();
+            if (correctPosition == 0)
+                this.HandleCorrectAnswer();
+            else
+                this.HandleIncorrectAnswer();
+        }        
     }
 
     public void OnMiddleClick()
     {
-        if (correctPosition == 1)
+        if (!this.isAudioPlaying)
         {
-            audioCorrecto.Play();
-            StartCoroutine(DoNextAfterDelay(1.488f));
+            if (correctPosition == 1)
+                this.HandleCorrectAnswer();
+            else
+                this.HandleIncorrectAnswer();
         }
     }
 
     public void OnRightClick()
     {
-        if (correctPosition == 2)
+        if (!this.isAudioPlaying)
         {
-            audioCorrecto.Play();
-            StartCoroutine(DoNextAfterDelay(1.488f));
-        }
+            if (correctPosition == 2)
+                this.HandleCorrectAnswer();
+            else
+                this.HandleIncorrectAnswer();
+        }        
+    }
+
+    private void HandleCorrectAnswer()
+    {
+        this.isAudioPlaying = true;
+        audioCorrecto.Play();
+        StartCoroutine(DoNextAfterDelay(1.488f));
+
+        DatabaseReference reference = currentGameReference.Child("intento").Push();
+        reference.Child("tiempo").SetValueAsync(timerString);
+        reference.Child("error").SetValueAsync(false);
+    }
+
+    private void HandleIncorrectAnswer()
+    {
+        this.audioFallo.Play();
+        DatabaseReference reference =  currentGameReference.Child("intento").Push();
+        reference.Child("tiempo").SetValueAsync(timerString);
+        reference.Child("error").SetValueAsync(true);
+    }
+
+    IEnumerator DoNextAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        this.Next();
+        this.isAudioPlaying = false;
     }
 
     private class Answer
     {
         public string image;
         public string name;
+        public AudioSource audio;
 
-        public Answer(string image, string name)
+        public Answer(string image, string name, AudioSource audio)
         {
             this.image = image;
             this.name = name;
+            this.audio = audio;
         }
     }
 }
---
+++ Assets/scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+    public void OpenAprender()
+    {
+        SceneManager.LoadScene("Aprender");
+    }
+
+    public void OpenActivades1al10()
+    {
+        SceneManager.LoadScene("ActividadesPrincipal");
+    }
+
+    public void OpenActivades11al20()
+    {
+        SceneManager.LoadScene("ActividadesPrincipalOnce");
+    }
+
+    public void OpenPracticar()
+    {
+        SceneManager.LoadScene("PracticarNumeros1");
+    }
+}
