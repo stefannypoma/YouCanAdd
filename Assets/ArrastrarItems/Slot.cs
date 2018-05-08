7a0c06cbd86cbca32625c19bbd85784aebf23b37
stefannypoma <stefanny.pomab@gmail.com>
2018-05-08 16:42:59 -0500

    agregar escena Arrastrar

---
+++ Assets/ArrastrarItems/Slot.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour, IDropHandler
+{
+    public GameObject item
+    {
+        get
+        {
+            if (transform.childCount > 0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+            return null;
+        }
+    }
+
+    #region IDropHandler implementation
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (!item)
+        {
+            DragHandeler.itemBeingDragged.transform.SetParent(transform);
+        }
+    }
+    #endregion
+}
