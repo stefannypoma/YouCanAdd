175d5686153f363aa7d263347504a9c888af55cb
Stefanny Poma <stefannypoma@iMac-de-mac.local>
2018-05-09 16:32:16 -0500

    agregacion de arrastre numeroas hasta el 10

---
+++ Assets/scripts/ArrastrarScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ArrastrarScript : MonoBehaviour {
+
+	public void openArrastrar2()
+	{
+		SceneManager.LoadScene("Arrastrar2");
+	}
+
+	public void openArrastrar3()
+	{
+		SceneManager.LoadScene("Arrastrar3");
+	}
+
+	public void openArrastrar4()
+	{
+		SceneManager.LoadScene("Arrastrar4");
+	}
+
+	public void openArrastrar5()
+	{
+		SceneManager.LoadScene("Arrastrar5");
+	}
+
+	public void openArrastrar6()
+	{
+		SceneManager.LoadScene("Arrastrar6");
+	}
+
+	public void openArrastrar7()
+	{
+		SceneManager.LoadScene("Arrastrar7");
+	}
+
+	public void openArrastrar8()
+	{
+		SceneManager.LoadScene("Arrastrar8");
+	}
+
+	public void openArrastrar9()
+	{
+		SceneManager.LoadScene("Arrastrar9");
+	}
+
+	public void openArrastrar10()
+	{
+		SceneManager.LoadScene("Arrastrar10");
+	}
+
+}
